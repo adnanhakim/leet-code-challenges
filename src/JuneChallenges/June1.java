601e7ae35e39817e3cdb382ec2fa6746c763e41f
Adnan Hakim <adnanhakim007@gmail.com>
2020-06-01 20:50:32 +0530

    Completed June 1

---
+++ src/JuneChallenges/June1.java
@@ -0,0 +1,45 @@
+package JuneChallenges;
+
+/*
+June 1
+
+Question: Invert Binary Tree
+Invert a binary tree.
+
+https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3347/
+*/
+
+class June1 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return root;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
