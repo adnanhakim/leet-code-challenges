cb04decfd4edc89631b6f9bdf45a371704ba6c8f
Adnan Hakim <adnanhakim007@gmail.com>
2020-07-06 23:45:52 +0530

    Completed July 6

---
+++ src/JulyChallenges/July6.java
@@ -0,0 +1,29 @@
+package JulyChallenges;
+
+/*
+July 6
+
+Question: Plus One
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3382/
+*/
+
+class July6 {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
