d019acffc0a111c4080b386d5b366622c690b935
Adnan Hakim <adnanhakim007@gmail.com>
2020-07-22 16:50:32 +0530

    Completed July 22

---
+++ src/JulyChallenges/July22.java
@@ -0,0 +1,68 @@
+package JulyChallenges;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+July 22
+
+Question: Binary Tree Zigzag Level Order Traversal
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+https://leetcode.com/explore/featured/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3397/
+*/
+
+class July22 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            List<Integer> level = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+                if (count % 2 != 0)
+                    level.add(0, curr.val);
+                else level.add(curr.val);
+            }
+            if (level.size() > 0)
+                levels.add(level);
+
+            count++;
+        }
+
+        return levels;
+    }
+}
