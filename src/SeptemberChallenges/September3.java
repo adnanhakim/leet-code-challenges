e7179222b0ca26108c68505e67068df1533df435
Adnan Hakim <adnanhakim007@gmail.com>
2020-09-05 00:23:13 +0530

    Completed September 3

---
+++ src/SeptemberChallenges/September3.java
@@ -0,0 +1,32 @@
+package SeptemberChallenges;
+
+/*
+September 3
+
+Question: Repeated Substring Pattern
+Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+
+https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3447/
+*/
+
+public class September3 {
+    public boolean repeatedSubstringPattern(String s) {
+        int stringLength = s.length();
+        int repeatLength = 1;
+
+        for (int i = stringLength / 2; i > 0; i--) {
+            if (stringLength % i != 0)
+                continue;
+            repeatLength = i;
+            if (s.substring(0, repeatLength - 1).equals(s.substring(repeatLength, repeatLength + repeatLength - 1)))
+                break;
+        }
+
+        String repeatedString = s.substring(0, repeatLength);
+        for (int i = repeatLength; i <= stringLength - repeatLength; i += repeatLength)
+            if (!repeatedString.equals(s.substring(i, i + repeatLength)))
+                return false;
+
+        return repeatLength != stringLength;
+    }
+}
